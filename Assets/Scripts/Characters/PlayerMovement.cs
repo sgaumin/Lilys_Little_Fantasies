b93cbd113a62d66b503f114f4cb2d3b4c1cd9ac2
Baptiste Bourgoin <baptiste.bourgoin@ext.uni.lu>
2019-07-07 18:17:05 +0200

    Added Brush System

--- Assets/Scripts/Characters/PlayerMovement.cs
+++ Assets/Scripts/Characters/PlayerMovement.cs
@@ -10,11 +10,18 @@ public class PlayerMovement : MonoBehaviour
 
 	public float runSpeed = 40f;
 
+	public PaintScript paintPrefab;
+
+	[SerializeField] private Transform particleSpawn;
+
 	float horizontalMove = 0f;
 	bool jump = false;
 	bool crouch = false;
 
 	private SpriteRenderer spriteRenderer;
+
+	private int numberOfParticles;
+	private int MAX_numberOfParticles;
 
 	protected void Start() => spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 
@@ -32,6 +39,9 @@ public class PlayerMovement : MonoBehaviour
 		if (Input.GetButtonDown("Attack"))
 		{
 			animator?.SetTrigger("Attack");
+			numberOfParticles = 0;
+			MAX_numberOfParticles = 10;
+			StartCoroutine(SpawnParticles());
 		}
 	}
 
@@ -51,5 +61,19 @@ public class PlayerMovement : MonoBehaviour
 	{
 		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
 		jump = false;
+	}
+
+	private IEnumerator SpawnParticles()
+	{
+		numberOfParticles++;
+		PaintScript paint;
+
+		for (int i = 0; i < MAX_numberOfParticles; i++)
+		{
+			yield return new WaitForSeconds(0.05f);
+			paint = Instantiate(paintPrefab, particleSpawn.position + new Vector3(0, 0.5f - 0.15f * numberOfParticles), Quaternion.identity);
+			paint.Launch(new Vector2(0.45f * Random.value * transform.localScale.x, 0.45f * Random.value));
+			numberOfParticles++;
+		}
 	}
 }
--- Assets/Scripts/Effects/PaintScript.cs
+++ Assets/Scripts/Effects/PaintScript.cs
@@ -1,37 +1,46 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PaintScript : MonoBehaviour
 {
 	[SerializeField] private SpriteRenderer sprite;
 	[SerializeField] private BoxCollider2D collider;
+	[SerializeField] private Rigidbody2D rigidbody2D;
+	[SerializeField] float force;
+	[SerializeField] MovingInDirection moving;
 
 	private bool isMoving;
 	private float time;
+	Tweener anim;
 	// Start is called before the first frame update
 	void Start()
     {
 		time = 0;
 		isMoving = true;
-
+		moving.enabled = false;
+		anim = sprite.transform.DORotate(new Vector3(0, 0, 360), 0.8f, RotateMode.FastBeyond360).SetLoops(-1);
+		anim.Play();
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		time += Time.deltaTime;
-		if (isMoving)
-		{
-			this.gameObject.transform.position += new Vector3(3f, 0.5f, 0f) * Time.deltaTime;
-			this.gameObject.transform.Rotate(new Vector3(0f, -90 / Time.deltaTime));
-		}
-	}
-
-	private void OnTriggerCollider2D(GameObject gameObject)
+	void OnTriggerEnter2D(Collider2D collider2D)
 	{
-		isMoving = false;
-		if (gameObject.CompareTag("Lava"))
+		if (collider2D.gameObject.layer==LayerMask.NameToLayer("Ground"))
+		{
+			rigidbody2D.bodyType = RigidbodyType2D.Static;
+			moving.enabled = true;
+			anim.Pause();
+			anim.Kill();
+		}
+
+		if (collider2D.gameObject.CompareTag("Destructor")||collider2D.gameObject.CompareTag("Lava"))
+		{
+			anim.Kill();
+			Destroy(gameObject);
+		}
+		/*if (gameObject.CompareTag("Lava"))
 		{
 			this.enabled = false;
 		}
@@ -39,6 +48,11 @@ public class PaintScript : MonoBehaviour
 		{
 			//gameObject.Hit();
 			this.enabled = false;
-		}
+		}*/
+	}
+
+	public void Launch(Vector2 direction)
+	{
+		rigidbody2D.AddForce(direction*force);
 	}
 }
