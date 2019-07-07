62a7ade3496f09ecd021647b816533db2633b512
Sebastien Gaumin <sgaumin@gmail.com>
2019-07-07 11:50:46 +0200

    Added enemy spawner to chunk prefabs.

---
+++ Assets/Scripts/Managers/LevelGeneration/MonsterSpawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+	[SerializeField] private Monster[] monsterPrefabs;
+
+	private void Start()
+		=> Instantiate(monsterPrefabs[Random.Range(0, monsterPrefabs.Length)], transform.position, Quaternion.identity, transform);
+}
