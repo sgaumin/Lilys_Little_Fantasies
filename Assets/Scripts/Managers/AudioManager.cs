4d352e72ba6f27feee77b98f7e12b140ef97d6d6
Sebastien Gaumin <sgaumin@gmail.com>
2019-07-07 23:10:49 +0200

    Integrated musics.

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+	[SerializeField] private AudioClip[] musics;
+
+	private AudioSource audioSource;
+
+	protected void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+		audioSource.clip = musics[Random.Range(0, musics.Length)];
+		audioSource.Play();
+	}
+}
