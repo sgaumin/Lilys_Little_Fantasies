fbeeee92bc05d0a411ae95b9abec0d4d095dd200
Sebastien Gaumin <sgaumin@gmail.com>
2019-07-07 12:58:39 +0200

    Added lava prefab into tilemap.

---
+++ Assets/Scripts/Objects/Lava.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+	[SerializeField] private float forceAmount;
+	[SerializeField] private float animSpeedDifference;
+
+	private Animator anim;
+
+	private void Start()
+	{
+		anim = GetComponentInChildren<Animator>();
+		anim.speed = Random.Range(1f - animSpeedDifference, 1f + animSpeedDifference);
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+			rb?.AddForce(Vector2.up * forceAmount);
+		}
+	}
+}
