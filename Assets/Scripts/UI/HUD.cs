f676dc0089bd136ea2a88c80a96b3e79814d36ea
Baptiste Bourgoin <baptiste.bourgoin@uni.lu>
2019-07-07 22:22:52 +0200

    HUD updated

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -3,15 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
 	public static HUD Instance { get; private set; }
 
 	[SerializeField] private TextMeshProUGUI timer;
+	[SerializeField] private Slider insanityBar;
+	[SerializeField] private InsanityText insanityText;
 
 	private float time;
 	private Sequence timerSequence;
+	private Sequence animBarText;
 	private bool timerWarning;
 
 	protected void Awake() => Instance = this;
@@ -20,9 +24,14 @@ public class HUD : MonoBehaviour
 	{
 		Tween timerAnim1 = timer.DOColor(Color.red, 0.1f);
 		Tween timerAnim2 = timer.DOColor(Color.white, 0.1f);
+
 		timerSequence = DOTween.Sequence();
 		timerSequence.Append(timerAnim1).Append(timerAnim2).Insert(0f, timer.transform.DOScale(1.2f, 0.2f)).Insert(0f, timer.transform.DOScale(1f, 0.2f)).SetLoops(-1);
+		timerSequence.Pause();
 		timerWarning = false;
+
+		//TO ERASE
+		editInsanityBar(0.5f);
 	}
 
 	protected void Update()
@@ -34,10 +43,40 @@ public class HUD : MonoBehaviour
 
 		timer.text = minutes + ":" + seconds;
 
+
+		if (Mathf.Floor(time % 60) >= 5f && !timerWarning)
+		{
+			editInsanityBar(0.5f);
+		}
+		if (Mathf.Floor(time % 60) >= 8f && !timerWarning)
+		{
+			editInsanityBar(0.1f);
+		}
+		if (Mathf.Floor(time % 60) >= 10f && !timerWarning)
+		{
+			editInsanityBar(0.9f);
+		}
 		if (Mathf.Floor(time % 60) >= 45f && !timerWarning)
 		{
 			timerWarning = true;
 			timerSequence.Play();
 		}
+		if (insanityBar.value==0f)
+		{
+			Debug.Log("GameOver Scene + return main Screen");
+		}
+		if (insanityBar.value<=0.25)
+		{
+			insanityText.switchState(false);
+		}
+		else
+		{
+			insanityText.switchState(true);
+		}
+	}
+
+	public void editInsanityBar(float value)
+	{
+		insanityBar.DOValue(value, 0.5f).Play();
 	}
 }
--- Assets/Scripts/UI/InsanityText.cs
+++ Assets/Scripts/UI/InsanityText.cs
@@ -7,21 +7,42 @@ using DG.Tweening;
 public class InsanityText : MonoBehaviour
 {
 	[SerializeField] private TextMeshProUGUI text;
-    // Start is called before the first frame update
-    void Start()
+	Sequence sequence1;
+	Sequence animBarText;
+	// Start is called before the first frame update
+	void Start()
     {
 		DOTween.defaultAutoPlay = AutoPlay.None;
 		DOTween.defaultAutoKill = false;
 		Tween anim1 = text.DOColor(new Color(183 / 255f, 113 / 255f, 181 / 255f), 1.25f);
 		Tween anim2 = text.DOColor(Color.white, 0.3f);
-		Sequence sequence1 = DOTween.Sequence();
+		sequence1 = DOTween.Sequence();
 		sequence1.Append(anim1).Append(anim2).SetLoops(-1);
 		sequence1.PlayForward();
-    }
+
+		Tweener anim21 = text.DOColor(Color.red, 0.1f);
+		Tweener anim22 = text.DOColor(Color.white, 0.1f);
+		animBarText = DOTween.Sequence();
+		animBarText.Append(anim21).Append(anim22).Insert(0f, text.transform.DOScale(1.2f, 0.2f)).Insert(0f, text.transform.DOScale(1f, 0.2f)).SetLoops(-1).Pause();
+	}
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+	public void switchState(bool value)
+	{
+		if (value)
+		{
+			sequence1.PlayForward();
+			animBarText.Pause();
+		}
+		else
+		{
+			sequence1.Pause();
+			animBarText.PlayForward();
+		}
+	}
 }
