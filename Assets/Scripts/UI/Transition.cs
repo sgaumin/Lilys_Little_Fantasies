87f74b4038e52802b03254bd6ed5bba5112f30e0
Sebastien Gaumin <sgaumin@gmail.com>
2019-07-08 21:37:44 +0200

    Updated HUD & transitions.

--- Assets/Scripts/UI/Transition.cs
+++ Assets/Scripts/UI/Transition.cs
@@ -8,11 +8,29 @@ public class Transition : MonoBehaviour
 {
 	public static Transition Instance { get; private set; }
 
-	[SerializeField] private Image image;
+	private Image image;
 
-	private void Awake() => Instance = this;
+	private void Awake()
+	{
+		Instance = this;
+		image = GetComponentInChildren<Image>();
+	}
 
-	private void Start() => image = GetComponentInChildren<Image>();
+	public void FadOut()
+	{
+		image.color = Color.clear;
+		image.DOFade(1f, 1f).Play();
+	}
 
-	public void FadOut() => image.DOFade(1f, 1f).Play();
+	public void FadOutWhite()
+	{
+		image.color = new Color(1f, 1f, 1f, 0f);
+		image.DOFade(1f, 0.4f).Play();
+	}
+
+	public void FadIn()
+	{
+		image.color = Color.black;
+		image.DOFade(0f, 1f).Play();
+	}
 }
