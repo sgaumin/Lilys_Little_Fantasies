815918bc502736ac65b7bd8736c0a65589ebf06d
Sebastien Gaumin <sebastien.gaumin@ext.uni.lu>
2019-07-12 18:24:31 +0200

    [Refacto] Renamed some variables.

--- Assets/Scripts/Tools/AudioExpress.cs
+++ Assets/Scripts/Tools/AudioExpress.cs
@@ -5,28 +5,32 @@ using Random = UnityEngine.Random;
 [Serializable]
 public class AudioExpress
 {
+	[SerializeField] private bool isUsingClips;
 	[SerializeField] private AudioClip clip;
+	[SerializeField] private AudioClip[] clips;
 	[SerializeField] private bool attached = true;
-	[SerializeField] private bool pitchModify;
+	[SerializeField] private bool loop;
+	[SerializeField] private bool isPitchModified;
 	[SerializeField, Range(0f, 1f)] private float pitchMaxVariation = 0.3f;
 
 	// ==== TODO ====
 	// Play sound when scene finish - Add a DontDestroyOnLoad script
-	// Destroy after time - Creatio. of a script
+	// Destroy after time - Creation of a script
 	// Create a pool of AudioSource or Stock them as a child of an empty object if not attached.
 
-	public void Play(GameObject objectAudioDemand = null)
+	public void Play(GameObject gameObject = null)
 	{
 		// Initialization
 		AudioSource audioSource;
 		audioSource = attached ?
-			objectAudioDemand.AddComponent<AudioSource>() :
+			gameObject.AddComponent<AudioSource>() :
 			new GameObject("Audio", typeof(AudioSource)).GetComponent<AudioSource>();
 
 		// Setup Paramaters
-		audioSource.clip = clip;
+		audioSource.clip = isUsingClips ? clips[Random.Range(0, clips.Length)] : clip;
 		audioSource.playOnAwake = false;
-		if (pitchModify)
+		audioSource.loop = loop;
+		if (isPitchModified)
 		{
 			audioSource.pitch -= Random.Range(0f, pitchMaxVariation);
 		}
