ea12db28d13878334728e9f0d03ca7d23ab36861
Sebastien Gaumin <sebastien.gaumin@ext.uni.lu>
2019-07-12 18:10:18 +0200

    Updated AudioExpress.

---
+++ Assets/Scripts/Tools/AudioExpress.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public class AudioExpress
+{
+	[SerializeField] private AudioClip clip;
+	[SerializeField] private bool attached = true;
+	[SerializeField] private bool pitchModify;
+	[SerializeField, Range(0f, 1f)] private float pitchMaxVariation = 0.3f;
+
+	// ==== TODO ====
+	// Play sound when scene finish - Add a DontDestroyOnLoad script
+	// Destroy after time - Creatio. of a script
+	// Create a pool of AudioSource or Stock them as a child of an empty object if not attached.
+
+	public void Play(GameObject objectAudioDemand = null)
+	{
+		// Initialization
+		AudioSource audioSource;
+		audioSource = attached ?
+			objectAudioDemand.AddComponent<AudioSource>() :
+			new GameObject("Audio", typeof(AudioSource)).GetComponent<AudioSource>();
+
+		// Setup Paramaters
+		audioSource.clip = clip;
+		audioSource.playOnAwake = false;
+		if (pitchModify)
+		{
+			audioSource.pitch -= Random.Range(0f, pitchMaxVariation);
+		}
+
+		// Play Sound
+		audioSource.Play();
+	}
+}
